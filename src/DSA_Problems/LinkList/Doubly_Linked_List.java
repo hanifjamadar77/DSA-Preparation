9e3b7c1fe658e0fb0cd5fd0ce4a14e031cb5d497
hanifjamadar77 <jamadarmhanif@gmail.com>
2025-08-28 23:47:16 +0530

    Doubly LL

---
+++ src/DSA_Problems/LinkList/Doubly_Linked_List.java
@@ -0,0 +1,99 @@
+package DSA_Problems.LinkList;
+
+public class Doubly_Linked_List {
+    private Node head;
+//    private Node tail;
+    private Node prev;
+    int size;
+
+    class Node{
+        Node next;
+//        Node tail;
+        Node prev;
+        int value;
+
+        Node(int value){
+            this.value = value;
+        }
+        Node (int value,Node next,Node prev){
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    public void insertFirst(int value){
+        Node node = new Node(value);
+        node.next = head;
+        node.prev = null;
+        if(head != null){
+            head.prev = node;
+        }
+        head = node;
+    }
+
+
+    public void insertLast(int value){
+        Node node = new Node(value);
+        Node last = head;
+
+        if(head == null){
+            node.prev = null;
+            head = node;
+            return;
+        }
+        while(last.next != null){
+            last = last.next;
+        }
+
+        last.next = node;
+        node.next = null;
+        node.prev = last;
+    }
+
+    public void insert(int value, int index){
+        Node p = find(index);
+        if(p == null){
+            System.out.println("Does not exist");
+            return;
+        }
+        Node node = new Node(value);
+        node.next = p.next;
+        p.next = node;
+        node.prev = p;
+        if(node.next != null){
+            node.next.prev = node;
+        }
+    }
+
+
+    public Node find(int value){
+        Node node = head;
+        while(node != null){
+            if(node.value == value){
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+
+    public void display(){
+        Node node = head;
+        Node last = null;
+        while(node != null){
+            System.out.print(node.value + " -> ");
+            last = node;
+            node = node.next;
+        }
+        System.out.println("END");
+
+        System.out.println("Printing Reverse");
+        while(last != null){
+            System.out.print(last.value + " -> ");
+            last = last.prev;
+        }
+        System.out.println("Start");
+    }
+}
--- src/DSA_Problems/LinkList/main.java
+++ src/DSA_Problems/LinkList/main.java
@@ -3,19 +3,35 @@ package DSA_Problems.LinkList;
 public class main {
     public static void main(String[] args) {
         Singly_Linked_List ll = new Singly_Linked_List();
-        ll.insertFirst(4);
-        ll.insertFirst(5);
-        ll.insertFirst(7);
-        ll.insertFirst(10);
+//        ll.insertFirst(4);
+//        ll.insertFirst(5);
+//        ll.insertFirst(7);
+//        ll.insertFirst(10);
+//
+//        ll.insertLast(11);
+//
+//        ll.insert(20,2);
+//
+//        ll.deleteFirst();
+//        ll.deleteLast();
+//        ll.delete(1);
+//
+//        ll.display();
 
-        ll.insertLast(11);
+        Doubly_Linked_List dll = new Doubly_Linked_List();
 
-        ll.insert(20,2);
+        dll.insertFirst(3);
+        dll.insertFirst(4);
+        dll.insertFirst(2);
+        dll.insertFirst(1);
+        dll.insertFirst(7);
 
-        ll.deleteFirst();
-        ll.deleteLast();
-        ll.delete(1);
+        dll.insertLast(10);
+        dll.insertLast(8);
 
-        ll.display();
+        dll.insert(11,2);
+
+        dll.display();
+
     }
 }
