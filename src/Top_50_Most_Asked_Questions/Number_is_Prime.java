9d0a930efbb8044464f601aa02a50a3f7e1892e4
hanifjamadar77 <jamadarmhanif@gmail.com>
2026-09-07 16:22:14 +0530

    Check if a Number is Prime

---
+++ src/Top_50_Most_Asked_Questions/Number_is_Prime.java
@@ -0,0 +1,39 @@
+package Top_50_Most_Asked_Questions;
+
+// Check if a Number is Prime
+
+// Time: O(√n)
+// Space: O(1)
+
+// Why i * i <= n?
+
+// If n has a factor greater than √n, it must also have a factor smaller than √n.
+
+public class Number_is_Prime {
+
+    public static boolean isPrime(int num){
+        if(num <= 1){
+            return false;
+        }
+
+        if(num == 2){
+            return true;
+        }
+
+        if(num % 2 == 0){
+            return false;
+        }
+
+        for(int i = 3; i * i <= num; i+= 2){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(17)); // true
+        System.out.println(isPrime(18)); // false
+    }
+}
