39ae504b6e208edabe46fd054d8c10dc018e9f81
hanifjamadar77 <jamadarmhanif@gmail.com>
2026-09-07 15:24:29 +0530

    Check if a String is a Palindrome

---
+++ src/Top_50_Most_Asked_Questions/String_Palindrome.java
@@ -0,0 +1,29 @@
+package Top_50_Most_Asked_Questions;
+
+// Check if a String is a Palindrome
+
+// Time: O(n)
+// Space: O(1)
+
+public class String_Palindrome {
+
+    public static Boolean palindrome(String str){
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while(start < end){
+            if(str.charAt(start) != str.charAt(end)){
+                return false;
+            }
+            start ++;
+            end --;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(palindrome("madam")); // True
+        System.out.println(palindrome("Hanif")); // False
+    }
+}
