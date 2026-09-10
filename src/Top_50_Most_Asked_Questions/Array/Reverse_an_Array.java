ac550c22eec227b600549f5d492c60d5d4cb77e9
hanifjamadar77 <jamadarmhanif@gmail.com>
2026-09-10 22:25:58 +0530

    Reverse an Array

---
+++ src/Top_50_Most_Asked_Questions/Array/Reverse_an_Array.java
@@ -0,0 +1,42 @@
+package Top_50_Most_Asked_Questions.Array;
+
+// Reverse an Array
+public class Reverse_an_Array {
+    public static void reverse(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+
+        // Continue until pointers meet
+        while(start < end){
+            // Swap elements
+            int temp = arr[start] ;
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            // Swap elements using the addition and substraction
+//            arr[start] = arr[start] + arr[end];
+//            arr[end]   = arr[start] - arr[end];
+//            arr[start] = arr[start] - arr[end];
+
+            // Swap elements using the XOR operator
+//            arr[start] = arr[start] ^ arr[end];
+//            arr[end]   = arr[start] ^ arr[end];
+//            arr[start] = arr[start] ^ arr[end];
+
+            // Move toward the center
+            start ++;
+            end --;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 2, 5, 9, 20};
+
+        reverse(arr);
+
+        // Print reversed array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
